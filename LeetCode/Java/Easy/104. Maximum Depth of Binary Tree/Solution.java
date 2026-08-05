86fb10910abec403667aeda3b2e7fa3b7a6e04f4
poornima-cys <2k23.cscys2311462@gmail.com>
2026-08-05 21:15:38 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/104. Maximum Depth of Binary Tree/Solution.java
+++ LeetCode/Java/Easy/104. Maximum Depth of Binary Tree/Solution.java
@@ -15,34 +15,14 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        // METHOD 1 -> USING BFS
-        // int levels=0;
-        // //int max_level=0;
-        // Queue<TreeNode>q=new LinkedList<>();
-        // if(root==null)
-        // return 0;
-        // q.offer(root);
-        // while(!q.isEmpty()){
-        //     int each_level_nodes=q.size();
-        //     for(int i=0; i<each_level_nodes;i++){
-        //         if(q.peek().left!=null){
-        //             q.offer(q.peek().left);
-        //         }
-        //          if(q.peek().right!=null){
-        //             q.offer(q.peek().right);
-        //         }
-        //         q.poll();
-        //     }
-        //     levels++;
-        //     //max_levels=Math.max(max_level, levels);
-        // }
-        //     return levels;
-        
-        
-        //METHOD 2 -> USING RECURSION
-        if(root==null){
+        if (root==null){
             return 0;
         }
-        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+        //now calculate left subtree height
+        int left = maxDepth(root.left);
+        //calculate right subtree height
+        int right = maxDepth(root.right);
+
+        return 1+Math.max(left, right);
     }
 }
