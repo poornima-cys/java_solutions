574da14dd08f96e771ecd2a9fb1a22c6159dc128
poornima-cys <2k23.cscys2311462@gmail.com>
2026-08-08 21:23:52 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/414. Third Maximum Number/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        HashSet<Integer> hs=new HashSet<>();
+       for(int i:nums){
+        hs.add(i);
+       }
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        pq.addAll(hs);
+        if(3>nums.length){
+            return pq.poll();
+        }
+        int k=3;
+        while(k>1){
+            pq.poll();
+            k-=1;
+        }
+        return pq.poll();
+    }
+}
