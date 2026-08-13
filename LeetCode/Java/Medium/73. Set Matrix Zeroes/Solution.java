70f928925d90b8b638232bcdb8d57f1175ef89c1
poornima-cys <2k23.cscys2311462@gmail.com>
2026-08-13 16:51:31 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/73. Set Matrix Zeroes/Solution.java
+++ LeetCode/Java/Medium/73. Set Matrix Zeroes/Solution.java
@@ -5,24 +5,24 @@ class Solution {
        for(int i=0;i<matrix.length;i++){
         for(int j=0;j<matrix[0].length;j++){
             if(matrix[i][j]==0){
-                matrix[i][j]=Integer.MAX_VALUE +1;
+                matrix[i][j]=1999999999;
                 for(int k=0;k<col;k++){
                     if(matrix[i][k]!=0){
-                        matrix[i][k]= Integer.MAX_VALUE +1;
+                        matrix[i][k]= 1999999999;
                     }
-                }
+                
                     for(int l=0;l<row;l++){
                         if(matrix[l][j]!=0){
-                            matrix[l][j]=Integer.MAX_VALUE+1;
+                            matrix[l][j]=1999999999;
                         }
                     }
-                // }
+                 }
             }
          }
        } 
        for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                if(matrix[i][j]==Integer.MAX_VALUE+1){
+                if(matrix[i][j]==1999999999){
                     matrix[i][j]=0;
                 }
             }
