2335bfee887d3d65de4b33d8c7496cbb086cdc68
poornima-cys <2k23.cscys2311462@gmail.com>
2026-08-13 16:20:29 +0530

    Add Java Solution to Patterns/Hash Map & Hashing

---
+++ LeetCode/Patterns/Hash Map & Hashing/73. Set Matrix Zeroes/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int row=matrix.length;
+        int col=matrix[0].length;
+       for(int i=0;i<matrix.length;i++){
+        for(int j=0;j<matrix[0].length;j++){
+            if(matrix[i][j]==0){
+                matrix[i][j]=Integer.MAX_VALUE + 1;
+                for(int k=0;k<col;k++){
+                    if(matrix[i][k]!=0){
+                        matrix[i][k]= Integer.MAX_VALUE + 1;
+                    }
+                    for(int l=0;l<row;l++){
+                        if(matrix[l][j]!=0){
+                            matrix[l][j]=Integer.MAX_VALUE + 1 ;
+                        }
+                    }
+                }
+            }
+         }
+       } 
+       for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==Integer.MAX_VALUE + 1){
+                    matrix[i][j]=0;
+                }
+            }
+       }
+    }
+}
