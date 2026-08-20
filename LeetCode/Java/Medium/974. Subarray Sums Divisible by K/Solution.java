b2bc9029316a064c14db71370cc50fd88853dc02
poornima-cys <2k23.cscys2311462@gmail.com>
2026-08-20 10:15:59 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/974. Subarray Sums Divisible by K/Solution.java
+++ LeetCode/Java/Medium/974. Subarray Sums Divisible by K/Solution.java
@@ -1,26 +1,48 @@
+// class Solution {
+//     public int subarraysDivByK(int[] nums, int k) {
+//         int len=nums.length;
+//         int prefix=0;
+//         HashMap<Integer, Integer> hm=new HashMap<>();
+//         hm.put(0,1);
+//         int count=0;
+
+//         for(int i=0;i<len;i++){
+//             prefix+=nums[i];
+//             //int rem= ((prefix % k)+k)%k;
+//             int rem=prefix%k;
+//             if(hm.containsKey(rem)){
+//                 count+=hm.get(rem);
+//             }
+//             if(hm.containsKey(rem)){
+//                 hm.put(rem,(hm.getOrDefault(rem, 1)+1));
+//             }
+//         }
+//         return count;
+//     }
+// }
+// /* TC=O(n)
+// SC = O(n)*/
+
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        int len=nums.length;
-        int prefix=0;
-        HashMap<Integer, Integer> hm=new HashMap<>();
-        hm.put(0,1);
-        int count=0;
+        int prefix = 0;
+        int count = 0;
 
-        for(int i=0;i<len;i++){
-            prefix+=nums[i];
-            int rem= prefix % k;
-            if(hm.containsKey(rem)){
-                count+=hm.get(rem);
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            prefix += nums[i];
+
+            int rem = ((prefix % k) + k) % k;
+
+            if (hm.containsKey(rem)) {
+                count += hm.get(rem);
             }
-            if(hm.containsKey(rem)){
-                hm.put(rem,(hm.getOrDefault(rem, 1)+1));
-            }
-            else{
-                hm.put(rem,1);
-            }
+
+            hm.put(rem, hm.getOrDefault(rem, 0) + 1);
         }
+
         return count;
     }
 }
-/* TC=O(n)
-SC = O(n)*/
