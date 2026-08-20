d6323970fb81930175a04aa55ebad14366a79066
poornima-cys <2k23.cscys2311462@gmail.com>
2026-08-20 09:49:57 +0530

    Add Java Solution to Patterns/Trees

--- LeetCode/Patterns/Trees/110. Balanced Binary Tree/Solution.java
+++ LeetCode/Patterns/Trees/110. Balanced Binary Tree/Solution.java
@@ -1,22 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int height_check(TreeNode root){
-        if(root==null)
-        return 0;
-
-        int left=height_check(root.left);
-        if(left==-1)
-        return -1;
-        int right=height_check(root.right);
-        if(right==-1)
-        return -1;
-
-        if(Math.abs(left-right)>1)
-        return -1;
-
-        return Math.max(left, right)+1;
-    }
     public boolean isBalanced(TreeNode root) {
-     return height_check(root)!=-1;
-        
-     }   
+        return check(root)!=-1;
     }
+   public int check(TreeNode root){
+    if(root==null)
+    return 0;
+
+    int left=check(root.left);
+    if(left==-1)return -1;
+    int right=check(root.right);
+    if(right==-1) return -1;
+
+    if(Math.abs(left-right)>1) return -1;
+   return  Math.max(left, right)+1;
+   }
+}
