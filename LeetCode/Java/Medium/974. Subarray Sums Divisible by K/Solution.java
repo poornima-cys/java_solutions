65cbffbdc74da4069d39bea26a3a72bd3188d823
poornima-cys <2k23.cscys2311462@gmail.com>
2026-08-20 10:05:31 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/974. Subarray Sums Divisible by K/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int len=nums.length;
+        int prefix=0;
+        HashMap<Integer, Integer> hm=new HashMap<>();
+        hm.put(0,1);
+        int count=0;
+
+        for(int i=0;i<len;i++){
+            prefix+=nums[i];
+            int rem= prefix % k;
+            if(hm.containsKey(rem)){
+                count+=hm.get(rem);
+                hm.put(rem,(hm.getOrDefault(rem, 1)+1));
+            }
+            else{
+                hm.put(rem,1);
+            }
+        }
+        return count;
+    }
+}
+/* TC=O(n)
+SC = O(n)*/
