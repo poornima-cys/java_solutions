96c66d2ef6742636bdb3cef08326c58c5adc6e89
poornima-cys <2k23.cscys2311462@gmail.com>
2026-08-21 15:15:00 +0530

    Add Java Solution to Patterns/Two Pointers

---
+++ LeetCode/Patterns/Two Pointers/151. Reverse Words in a String/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String reverseWords(String s) {
+        String str=s.trim();
+        String ch[]=str.split(" ");
+       Collections.reverse(Arrays.asList(ch));
+        System.out.println(Arrays.toString(ch));
+        String res="";
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]==""){
+                continue;
+            }
+            else{
+                res+=ch[i];
+                res+=" ";
+                            }
+        }
+        return res.trim();
+    }
+}
