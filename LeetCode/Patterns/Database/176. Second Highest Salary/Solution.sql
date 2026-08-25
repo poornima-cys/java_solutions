678f2c3299518d8ecb5339c405c6cbeab958d7ec
poornima-cys <2k23.cscys2311462@gmail.com>
2026-08-25 16:28:48 +0530

    Add SQL Solution to Patterns/Database

--- LeetCode/Patterns/Database/176. Second Highest Salary/Solution.sql
+++ LeetCode/Patterns/Database/176. Second Highest Salary/Solution.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-SELECT  Distinct MAX(salary) as SecondHighestSalary 
+SELECT MAX(salary) as SecondHighestSalary 
 FROM Employee where salary<(Select max(salary) from employee);
