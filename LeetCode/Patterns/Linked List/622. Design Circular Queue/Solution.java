b1b0afbde1ded820bda9ce0a6763b54b285d4645
poornima-cys <2k23.cscys2311462@gmail.com>
2026-08-25 15:57:48 +0530

    Add Java Solution to Patterns/Linked List

---
+++ LeetCode/Patterns/Linked List/622. Design Circular Queue/Solution.java
@@ -0,0 +1,71 @@
+class MyCircularQueue {
+     int[] q;
+        int front;
+        int rear;
+        int count;
+        int size;
+
+    public MyCircularQueue(int k) {
+         q=new int[k];
+         front=0;
+         rear=0;
+         count=0;
+         size=k;
+    }
+    
+    public boolean enQueue(int value) {
+        if(count == size){
+            return false;
+        }
+        q[rear]=value;
+        rear=(rear + 1)%size;
+        count+=1;
+        return true;
+        
+    }
+    
+    public boolean deQueue() {
+        if(count==0){
+            return false;
+        }
+        front=(front+1)%size;
+        count-=1;
+        return true;
+        
+    }
+    
+    public int Front() {
+        if(count==0){
+            return -1;
+        }return q[front];
+
+        
+    }
+    
+    public int Rear() {
+        if(count==0){
+            return -1;
+        }
+        return q[(rear-1+size)%size];
+    }
+    
+    public boolean isEmpty() {
+        return count==0;
+        
+    }
+    
+    public boolean isFull() {
+        return count==size;
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
