7ed9310d63a5cd0dae6c9287103b1d8b3cef48db
poornima-cys <2k23.cscys2311462@gmail.com>
2026-09-08 16:43:56 +0530

    Add Medium Java Solution for 199. Binary Tree Right Side View from LeetCode

--- LeetCode/Java/Medium/199. Binary Tree Right Side View/Solution.java
+++ LeetCode/Java/Medium/199. Binary Tree Right Side View/Solution.java
@@ -14,22 +14,43 @@
  * }
  */
 class Solution {
-    ArrayList<Integer> arr= new ArrayList<>();
-    int max=Integer.MIN_VALUE;
-    public void traverse(TreeNode root, int level, int ml, ArrayList<Integer> arr) {
-        if(root == null) return;
-         if(level> ml) {
-            arr.add(root.val);
-            max = Math.max(max, level);
-        }
-        traverse(root.right, level+1, max, arr);
+    // ArrayList<Integer> arr= new ArrayList<>();
+    // int max=Integer.MIN_VALUE;
+    // public void traverse(TreeNode root, int level, int ml, ArrayList<Integer> arr) {
+    //     if(root == null) return;
+    //      if(level> ml) {
+    //         arr.add(root.val);
+    //         max = Math.max(max, level);
+    //     }
+    //     traverse(root.right, level+1, max, arr);
        
-        traverse(root.left, level+1, max, arr);
+    //     traverse(root.left, level+1, max, arr);
+    // }
+    // public List<Integer> rightSideView(TreeNode root) {
+    //     if(root == null) return new ArrayList();
+    //     int maxlevel = Integer.MIN_VALUE;
+    //     traverse(root, 0, maxlevel, arr);
+    //     return arr;
+    // }
+    List<Integer> list=new ArrayList<>();
+    List<Integer> ans=new ArrayList<>();
+    public void check(TreeNode root, int l){
+        if(root==null){
+            return ;
+        }
+        if(!list.contains(l)){
+            list.add(l);
+            ans.add(root.val);
+        }
+         check(root.right, l+1);
+        check(root.left, l+1);
+
     }
     public List<Integer> rightSideView(TreeNode root) {
-        if(root == null) return new ArrayList();
-        int maxlevel = Integer.MIN_VALUE;
-        traverse(root, 0, maxlevel, arr);
-        return arr;
+        if(root==null){
+            return new ArrayList<>();
+        }
+        check(root,0);
+        return ans;
     }
 }
