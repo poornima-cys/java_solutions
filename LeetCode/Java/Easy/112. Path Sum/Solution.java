f5c40235e1b22e8e749d1bb70024b23bd0271428
poornima-cys <2k23.cscys2311462@gmail.com>
2026-09-14 20:50:02 +0530

    Add Easy Java Solution for 112. Path Sum from LeetCode

---
+++ LeetCode/Java/Easy/112. Path Sum/Solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean help(TreeNode root, int target){
+        if(root==null) return false;
+        target-=root.val;
+        if(target==0)return true;
+        boolean left=help(root.left,target);
+        boolean right=help(root.right,target);
+        return left|| right;
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+      return help(root, targetSum);
+    }
+}
